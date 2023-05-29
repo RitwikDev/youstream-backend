d3f18a9e0f6021f509181b495feacdac09de2f8f
Ritwik Dev <43510943+RitwikDev@users.noreply.github.com>
2023-05-29 18:32:20 -0500

    Initial commit

---
+++ youstream/src/main/java/com/s1/youstream/backend/youstream/YouStreamApplication.java
@@ -0,0 +1,13 @@
+package com.s1.youstream.backend.youstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouStreamApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(YouStreamApplication.class, args);
+    }
+}
---
+++ youstream/src/test/java/com/s1/youstream/backend/youstream/YouStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s1.youstream.backend.youstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
